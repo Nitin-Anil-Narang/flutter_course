70b14a73f21b784571d21a4c9f899c4b45ea0c37
Nitin Narang <nn@Nitins-MacBook-Air.local>
2025-01-20 19:22:26 +0530

    Added code Example for BottomNavBar

--- 07_drawer_nav/lib/main.dart
+++ 07_drawer_nav/lib/main.dart
@@ -32,6 +32,19 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          //Used to add Bottom Nav
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label:
+                    "Home"), //Have to use BottomNavItem to add items to the widget
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Setting"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.exit_to_app), label: "Quit"),
+          ],
+        ),
       ),
     );
   }
